17cd760013815b9c57d4f8cead27d24b9ab37433
yoonsun124 <dbstjs0124@gmail.com>
2024-08-25 22:46:22 +0900

    vite 수정

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
+// export default defineConfig({
+//   base: './',
+//   plugins: [react()],
 
+//   server: {
+//     port: 3000
+//   },
+//   resolve: {
+//     alias: {
+//       src: "/src",
+//     }
+//   },   
+// })
+
+export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 3000
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
-  resolve: {
-    alias: {
-      src: "/src",
-    }
-  },   
-})
+});
